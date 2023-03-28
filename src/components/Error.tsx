4381f1ce80a99b36aa7f599bf1dabdae4e986940
raunak gurud <raunakgurud2121@gmail.com>
2023-03-28 16:56:39 +0530

    fix: github repo link in error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -3,7 +3,7 @@ import { GitHubLogo } from './Icons';
 
 function Error({ message }) {
   return (
-    <div className='flex h-[calc(100vh-50%)] items-start justify-center'>
+    <div className='flex h-screen items-start justify-center'>
       <div className='flex flex-row p-4 md:p-20'>
         <div className='mx-20 flex w-auto flex-col items-center justify-center'>
           <h3 className='text-6xl font-semibold md:text-9xl'>Oops!!!</h3>
@@ -12,7 +12,7 @@ function Error({ message }) {
           </p>
           <p className='mt-4 flex flex-row items-center rounded bg-gradient-to-r from-cyan-500 to-blue-500 p-2 ease-in hover:bg-gradient-to-r hover:from-indigo-500  hover:to-pink-500'>
             <a
-              href='https://github.com/raunakgurud09/music-app'
+              href='https://github.com/raunakgurud09/book-my-show-clone'
               target='_blank'
               className='text-small'
             >
